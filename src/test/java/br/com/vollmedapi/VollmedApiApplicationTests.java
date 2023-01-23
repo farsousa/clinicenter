0a9918260b112e15c523735ceaf56b0b1807bdec
farsousa <andersoncaj26@gmail.com>
2023-01-23 08:50:24 -0300

    estrutura inicial do projeto com as dependencias principais configuradas (spring web, lombok, spring dev tolls e validation)

---
+++ src/test/java/br/com/vollmedapi/VollmedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vollmedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollmedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
